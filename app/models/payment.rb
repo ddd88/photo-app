8f4767bb9463a0f6c53982383e4377b0e14edaa9
ddd88 <dalewalk10@gmail.com>
2018-05-01 21:17:18 +1000

    redeployment part 2

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,6 +3,8 @@ class Payment < ApplicationRecord
 	attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
 	belongs_to :user
 
+	token = params[:stripeToken ]
+
 	def self.month_options
 		Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
 	end
@@ -17,7 +19,8 @@ class Payment < ApplicationRecord
 		Stripe::Charge.create customer: customer.id,
 													amount: 1000,
 													description: 'Premium',
-													currency: 'aud'
+													currency: 'aud', 
+													source: token
 	end
 
 end
